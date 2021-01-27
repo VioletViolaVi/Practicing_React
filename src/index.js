12d8a96d02d9e98a1ada98bdafc194f36c45f8ea
VioletViolaVi <vivian.v18723@gmail.com>
2021-01-27 15:14:34 +0000

    Add unordered list using react.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from './App'; vivian commented this out
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // vivian commented this bit out
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <div>
+    <h1>Hello World!</h1> 
+    <p>This is a paragraph</p>
+    <ul> 
+      <li>red</li>
+      <li>yellow</li>
+      <li>green</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
