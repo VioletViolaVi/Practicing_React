e619443da2ce068a1e1f562fc4e6f141f76231f6
VioletViolaVi <vivian.v18723@gmail.com>
2021-01-28 17:28:31 +0000

    Change react content to information about myself.

---
+++ react_practice/src/AboutMe/VivGoals.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function VivGoals() {
+  return (
+    <div>
+      <h2
+        style={{
+          backgroundColor: "pink",
+          width: "7em",
+          fontFamily: "Times New Roman serif",
+        }}
+      >
+        Vivian's Goals
+      </h2>
+      <p>
+        Vivian's goals include becoming a Full Stack Web Developer who is highly
+        knowledgeable in a range of both{" "}
+        <span style={{ color: "purple" }}>Front End</span> and{" "}
+        <span style={{ color: "purple" }}>Back End</span> technologies. She
+        hopes to be able to create a range of ecommerce and gaming websites that
+        the entire global internet community will be able to enjoy and
+        participate in.
+      </p>
+    </div>
+  );
+}
+
+export default VivGoals;
--- react_practice/src/index.js
+++ react_practice/src/index.js
@@ -4,41 +4,69 @@ import "./index.css";
 // import App from './App'; vivian commented this out
 import reportWebVitals from "./reportWebVitals";
 
-import IntroMe from "./myPracticeFunctions/IntroMe";
+import VivGoals from "./AboutMe/VivGoals";
 
 // vivian's functions
-function FirstTry() {
+function WebTitle() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1
+        style={{
+          fontStyle: "italic",
+          textAlign: "center",
+        }}
+      >
+        Vivian
+      </h1>
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h2>Who is Vivian?</h2>
       <p>
-        This is a <span style={{ textDecoration: "underline" }}>paragraph</span>{" "}
+        Vivian is a Leeds Beckett University Graduate with a{" "}
+        <span style={{ textDecoration: "underline" }}>Diploma</span> in Software
+        Development attained from Code Institute. She is 26 years old and has
+        developed a strong passion for pursuing a career in Web Development.
       </p>
     </div>
   );
 }
 
-function ColourList() {
+function FavColours() {
   return (
-    <ul>
-      <li style={{ color: "red" }}>red</li>
-      <li style={{ color: "green" }}>green</li>
-      <li style={{ color: "yellow" }}>yellow</li>
-    </ul>
+    <div>
+      <h2>Vivian's favourite colours are the following:</h2>
+      <ul>
+        <li style={{ color: "red" }}>Red</li>
+        <li style={{ color: "green" }}>Green</li>
+        <li style={{ color: "yellow" }}>Yellow</li>
+        <li style={{ color: "pink" }}>Pink</li>
+        <li style={{ color: "blue" }}>Blue</li>
+        <li style={{ color: "orange" }}>Orange</li>
+        <li style={{ color: "grey" }}>Grey</li>
+      </ul>
+    </div>
   );
 }
 
-function SongPara() {
+function FaveSongLyrics() {
   return (
-    <p
-      style={{
-        fontStyle: "italic",
-        color: "blue",
-      }}
-    >
-      John Little, Little John. What a crying shame they named him wrong. 'Cause
-      he turned out mighty, big and strong! Great, big, Little John.
-    </p>
+    <div>
+      <h2>Vivian's favourite song lyrics goes like this:</h2>
+      <p
+        style={{
+          fontStyle: "italic",
+          color: "purple",
+        }}
+      >
+        John Little, Little John. What a crying shame they named him wrong.
+        'Cause he turned out mighty, big and strong! Great, big, Little John.
+      </p>
+    </div>
   );
 }
 
@@ -46,10 +74,11 @@ ReactDOM.render(
   <React.StrictMode>
     {/* <App /> vivian commented this bit out */}
     <div>
-      <FirstTry />
-      <ColourList />
-      <SongPara />
-      <IntroMe />
+      <WebTitle />
+      <Intro />
+      <FavColours />
+      <FaveSongLyrics />
+      <VivGoals />
     </div>
   </React.StrictMode>,
   document.getElementById("root")
